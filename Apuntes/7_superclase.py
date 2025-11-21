3833a836946956964ca90b57babcf901a5c9412c
Bryan05U <bryanalexis.carcamo@alumnos.ulagos.cl>
2025-11-21 18:09:26 -0300

    unos cambios más

---
+++ Apuntes/7_superclase.py
@@ -0,0 +1,9 @@
+class Vehículo:
+    def __init__(self, motor):
+        self.motor = motor
+    
+    def andar(self):
+        pass
+
+class Moto(Vehículo):
+    Vehículo.__init__
--- Ejercicios/Unidad 3/1_vehículos-hibridos.py
+++ Ejercicios/Unidad 3/1_vehículos-hibridos.py
@@ -1,37 +1,29 @@
-import time, random
-
-#################################################
 # Clase base de Vehículo con el método moverse()
 class Vehículo:
-    def moverse(self, nombre):
-        self.nombre = nombre
-#################################################
-    
-
-# Clase de Vehículo Terrestre
-class Terrestre(Vehículo):
     def moverse(self):
-        # Llamando la función self.rodar() 
+        pass
+
+# Vehículo Terrestre
+class Terrestre(Vehículo):
+    def rodar(self):
+        print(f"El vehículo terrestre {self.__class__.__name__} está rodando sobre la carretera")
+
+class Coche(Terrestre):
+    def moverse(self):
         self.rodar()
 
-    def rodar(self):
-        print(f"El vehículo terrestre {self.nombre} está rodando sobre la carretera")
 
 # Clase de Vehículo Acuático
 class Acuático(Vehículo):
-    def moverse(self):
-        # Llamando la función self.navegar()
-        self.navegar()
-    
     def navegar(self):
         print(f"El vehículo acuático {self.nombre} está navegando sobre el mar")
 
+class Moto_Acuática():
+    def moverse(self):
+        self.navegar()
+
 # Clase de Vehículo Aéreo
 class Aéreo(Vehículo):
-    def moverse(self):
-        # Llamando la función self.volar()
-        self.volar()
-    
     def volar(self):
         print(f"El vehículo aéreo {self.nombre} está volando en el aire")
 
@@ -47,9 +39,7 @@ class Híbrido(Vehículo):
     def navegar(self):
         print(f"El vehículo híbrido {self.nombre} está navegando sobre el mar")
 
-# Función de simulación de carrera
-def simular_carrera(vehículos):
-    print()
-
 # Creación de vehículos
-vehículo1 = Terrestre("Auto")
+carrera_de_vehículos = [
+    
+]
